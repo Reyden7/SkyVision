1ddfff68a3c5284f7641bd8e92aabde11100f92b
reyden <quentin.cordiero@neuf.fr>
2023-11-02 20:29:12 +0100

    First commit
    Installation et préparation du projet

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <h1>Mon Application React</h1>
+      <p>Bienvenue dans mon application React.</p>
+    </div>
+  );
+}
+
+export default App;
