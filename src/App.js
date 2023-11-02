3bb394ac72662e6bf4dfa2e553e86d85f49fa19b
reyden <quentin.cordiero@neuf.fr>
2023-11-02 20:45:01 +0100

    creation des routes et importations des dépendances

--- src/App.js
+++ src/App.js
@@ -1,11 +1,40 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Link, Routes } from 'react-router-dom';
+
+import './App.css';
+
+import Home from './Home';
+import About from './About';
+import Contact from './Contact';
+import NotFound from './NotFound';
 
 function App() {
   return (
-    <div>
-      <h1>Mon Application React</h1>
-      <p>Bienvenue dans mon application React.</p>
-    </div>
+    <Router>
+      <div>
+        <header>
+          <section>
+            <h1>react-starter</h1>
+            <h2>(React version 18.0.2)</h2>
+          </section>
+          <nav>
+            <ul>
+              <li><Link to="/">Home</Link></li>
+              <li><Link to="/about">About</Link></li>
+              <li><Link to="/contact">Contact</Link></li>
+            </ul>
+          </nav>
+        </header>
+        <main>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/contact" element={<Contact />} />
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        </main>
+      </div>
+    </Router>
   );
 }
 
