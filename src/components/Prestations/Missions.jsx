6c010a241dae31aebedfb6feed3d5b26c3983061
reyden <quentin.cordiero@neuf.fr>
2023-11-21 09:35:21 +0100

    supp aos

--- src/components/Prestations/Missions.jsx
+++ src/components/Prestations/Missions.jsx
@@ -21,16 +21,16 @@ function Missions() {
       <div className=''></div>
       <div className=" grid grid-cols-2 md:grid-cols-3 2xl:grid-cols-3  md:ml-40 2xl:ml-0   2xl:mr-96 ">
         <div  className=' w-auto md:w-60  2xl:w-72 lg:w-60 md:-ml-14 2xl:ml-44 3xl:ml-96    mx-auto'>
-          <img data-aos-duration="1000"  data-aos="flip-up" className='mt-0 2xl:mt-0 shadow-lg shadow-black' src={img1}  alt="Image 1" />
-          <img  data-aos-duration="2000" data-aos="flip-up" className='mt-4 2xl:mt-8 shadow-lg shadow-black' src={img2} alt="Image 2" />
-          <img data-aos-duration="3000"  data-aos="flip-up" className='mt-4 2xl:mt-8 shadow-lg shadow-black' src={img3} alt="Image 3" />
+          <img  className='mt-0 2xl:mt-0 shadow-lg shadow-black' src={img1}  alt="Image 1" />
+          <img   className='mt-4 2xl:mt-8 shadow-lg shadow-black' src={img2} alt="Image 2" />
+          <img  className='mt-4 2xl:mt-8 shadow-lg shadow-black' src={img3} alt="Image 3" />
         </div>
         <div className='w-auto 2xl:ml-0 md:w-60 2xl:w-80 lg:w-60 ml-4 lg:-ml-16 xl:-ml-52 3xl:ml-0    mx-auto 2xl:mt-40 3xl:-ml-52 mt-20'>
-          <img data-aos-duration="2500"  data-aos="flip-up" className='shadow-lg shadow-black' src={img4} alt="Image 4" />
-          <img data-aos-duration="1700"  data-aos="flip-up" className='2xl:mt-8 mt-4 shadow-lg shadow-black' src={img5} alt="Image 5" />
+          <img  className='shadow-lg shadow-black' src={img4} alt="Image 4" />
+          <img  className='2xl:mt-8 mt-4 shadow-lg shadow-black' src={img5} alt="Image 5" />
         </div>
-        <div data-aos-duration="2000" data-aos="fade-left" className='mt-56 mx-auto 2xl:block lg:block xl:-ml-56 2xl:ml-0 hidden  '>
-          <div className='text-neutral-800  font-signikaSemiBold 2xl:text-2xl'>
+        <div  className='mt-56 mx-auto 2xl:block lg:block xl:-ml-56 2xl:ml-0 hidden  '>
+          <div className='text-neutral-800  font-signikaSemiBold text-2xl 2xl:text-2xl'>
             Nous intervenons pour différents types de mission :
           </div>
           <div className='font-signikaLight mt-5 text-neutral-800 2xl:text-xl '>
@@ -44,7 +44,7 @@ function Missions() {
           </div>
         </div>
       </div>
-      <div data-aos-duration="2000" data-aos="fade-left" className='mt-10 mx-auto 2xl:hidden lg:hidden block   '>
+      <div  className='mt-10 mx-auto 2xl:hidden lg:hidden block   '>
           <div className='text-neutral-800  font-signikaSemiBold 2xl:text-2xl'>
             Nous intervenons pour différents types de mission :
           </div>
